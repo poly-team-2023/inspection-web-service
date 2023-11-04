87c60da94efeec09a700415fe01256dd408ab537
paikeee <serzhik2002@gmail.com>
2023-11-04 21:10:35 +0300

    added repositories

---
+++ src/main/java/com/service/inspection/repositories/PhotoRepository.java
@@ -0,0 +1,12 @@
+package com.service.inspection.repositories;
+
+import com.service.inspection.entities.Photo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface PhotoRepository extends JpaRepository<Photo, UUID> {
+
+}
---
+++ src/main/java/com/service/inspection/repositories/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.service.inspection.repositories;
+
+import com.service.inspection.entities.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+
+}
