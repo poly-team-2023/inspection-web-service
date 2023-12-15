92257ee22bbe4025fb98691cc973650aef324ac5
paikeee <serzhik2002@gmail.com>
2023-12-15 17:59:19 +0300

    fixed dto (new date format)

--- src/main/java/com/service/inspection/dto/equipment/EquipmentDto.java
+++ src/main/java/com/service/inspection/dto/equipment/EquipmentDto.java
@@ -2,6 +2,10 @@ package com.service.inspection.dto.equipment;
 
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.validation.annotation.Validated;
 
 import jakarta.validation.constraints.NotBlank;
@@ -15,7 +19,11 @@ public class EquipmentDto {
     private String name;
     @NotBlank
     private String serialNumber;
+    private String verificationNumber;
     @NotNull
+    @JsonFormat(pattern = "dd.MM.yyyy")
+    @DateTimeFormat(pattern = "dd.MM.yyyy")
+    @JsonDeserialize(using = LocalDateDeserializer.class)
     private LocalDate verificationDate;
 
 }
--- src/main/java/com/service/inspection/dto/equipment/GetEquipmentDto.java
+++ src/main/java/com/service/inspection/dto/equipment/GetEquipmentDto.java
@@ -2,7 +2,9 @@ package com.service.inspection.dto.equipment;
 
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Data
 public class GetEquipmentDto {
@@ -10,6 +12,9 @@ public class GetEquipmentDto {
     private Long id;
     private String name;
     private String serialNumber;
+    private String verificationNumber;
+    @JsonFormat(pattern = "dd.MM.yyyy")
+    @DateTimeFormat(pattern = "dd.MM.yyy")
     private LocalDate verificationDate;
 
 }
