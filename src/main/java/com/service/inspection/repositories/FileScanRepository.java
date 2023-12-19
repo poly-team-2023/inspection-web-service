d33cc89f7de8b0e52981440a421ffcf145c49fcb
paikeee <serzhik2002@gmail.com>
2023-12-19 14:02:53 +0300

    adding new entity in db and project

---
+++ src/main/java/com/service/inspection/repositories/FileScanRepository.java
@@ -0,0 +1,17 @@
+package com.service.inspection.repositories;
+
+import com.service.inspection.entities.Company;
+import com.service.inspection.entities.FileScan;
+import com.service.inspection.entities.License;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface FileScanRepository extends JpaRepository<FileScan, Long> {
+
+    List<FileScan> findFileScanByCompany(Company company);
+
+    List<FileScan> findFileScanByLicense(License company);
+}
