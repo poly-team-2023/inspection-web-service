d95440db215dd9d88a0231e1009228cc01fcc9cf
paikeee <serzhik2002@gmail.com>
2023-12-19 14:04:19 +0300

    dto fix

--- src/main/java/com/service/inspection/dto/company/GetCompanyDto.java
+++ src/main/java/com/service/inspection/dto/company/GetCompanyDto.java
@@ -2,7 +2,9 @@ package com.service.inspection.dto.company;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.service.inspection.dto.employer.GetEmployerDto;
+import com.service.inspection.dto.files.GetFileScanDto;
 import com.service.inspection.dto.license.GetLicenseDto;
 
 import lombok.Data;
@@ -14,6 +16,8 @@ public class GetCompanyDto {
     private String name;
     private String legalAddress;
     private String city;
+    @JsonProperty(value = "sro")
+    private List<GetFileScanDto> filesSro;
     private List<GetEmployerDto> employers;
     private List<GetLicenseDto> licenses;
 }
--- src/main/java/com/service/inspection/dto/license/GetLicenseDto.java
+++ src/main/java/com/service/inspection/dto/license/GetLicenseDto.java
@@ -1,12 +1,17 @@
 package com.service.inspection.dto.license;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.service.inspection.dto.files.GetFileScanDto;
 import lombok.Data;
+
+import java.util.List;
 
 @Data
 public class GetLicenseDto {
 
     private Long id;
     private String name;
-    private Integer number;
+    @JsonProperty(value = "files")
+    private List<GetFileScanDto> files;
 
 }
