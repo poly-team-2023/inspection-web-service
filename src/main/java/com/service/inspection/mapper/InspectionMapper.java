4562495941eee5b1af73d78f2274555a1f7cdc82
Ivan Piatlin <piatlin2002@gmail.com>
2023-12-22 23:45:08 +0300

    future(Inspection.java): adding inspection isCultureField

--- src/main/java/com/service/inspection/mapper/InspectionMapper.java
+++ src/main/java/com/service/inspection/mapper/InspectionMapper.java
@@ -5,11 +5,14 @@ import com.service.inspection.dto.inspection.InspectionDto;
 import com.service.inspection.dto.inspection.InspectionWithName;
 import com.service.inspection.entities.Inspection;
 
+import com.service.inspection.entities.enums.BuildingType;
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
+
+import java.util.Objects;
 
 @Mapper(
         injectionStrategy = InjectionStrategy.CONSTRUCTOR,
@@ -22,8 +25,20 @@ public interface InspectionMapper {
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "company", source = "companyId", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.SET_TO_NULL)
     @Mapping(target = "employer", source = "employerId", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.SET_TO_NULL)
+    @Mapping(source = "isCulture", target = "buildingType")
     void mapToInspection(@MappingTarget Inspection inspection, InspectionDto inspectionDto);
 
 
     GetInspectionDto mapToGetInspectionDto(Inspection inspection);
+
+    default Boolean toIsCultureBuildingType(BuildingType buildingType) {
+        return Objects.equals(buildingType, BuildingType.CULTURE);
+    }
+
+    default BuildingType toBuildingType(Boolean isCulture) {
+        if (Boolean.TRUE.equals(isCulture)) {
+            return BuildingType.CULTURE;
+        }
+        return BuildingType.NOT_CULTURE;
+    }
 }
