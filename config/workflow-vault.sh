adf22d5e08bd76c0ca0fa9b8971477805f2b53f2
Andrey <70713475+Queenore@users.noreply.github.com>
2024-01-06 01:35:02 +0300

    vault deploy finally working

--- config/workflow-vault.sh
+++ config/workflow-vault.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
-#!/bin/sh
 
 # Start vault
 vault server -config vault.json &
-sleep 3
+sleep 2
 echo "Vault started"
-
 
 # Check if Vault is initialized
 if vault status | grep -q "Initialized\s*true"; then
-    echo "Vault has been uninitialized."
+    echo "Vault has been initialized."
 else
-    echo "Vault is not initialized."
+    echo "Vault is not uninitialized."
     vault operator init > generated_keys.txt
 fi
 
@@ -24,23 +22,25 @@ vault operator unseal "$(echo "$keyArray" | awk 'NR==3')"
 # Get root token
 rootToken=$(grep "Initial Root Token: " < generated_keys.txt | cut -c21-)
 echo "$rootToken" > root_token.txt
-
 export VAULT_TOKEN="$rootToken"
 
 # Enable kv
-vault secrets enable -version=1 kv
+#vault secrets enable -version=2 kv
 
 # Enable userpass and add default user
 #vault auth enable userpass
 #vault policy write spring-policy spring-policy.hcl
 #vault write auth/userpass/users/admin password="${SECRET_PASS}" policies=spring-policy
 
+## Put key-values
 secrets_file="secrets.txt"
-while IFS= read -r line; do
+secret_string=""
+while IFS= read -r line || [ -n "$line" ]; do
     if [[ "$line" != "#"* ]]; then
-        vault kv put kv/my-secret $line
+        secret_string="$secret_string $line"
     fi
 done < "$secrets_file"
+vault kv put kv/base $secret_string
 
 # Keep the script running to keep the Vault server alive
 trap "exit 0" SIGINT SIGTERM
